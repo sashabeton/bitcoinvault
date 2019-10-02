317bc8a6ff0acbe421e414117900d6414f4d4d99
Ian Duoteli Fleming <54151411+ianduoteli@users.noreply.github.com>
2019-10-02 22:29:15 +0000

    Testing: improve robustness of test

--- manual/2-start-network.sh
+++ manual/2-start-network.sh
@@ -2,8 +2,8 @@
 
 echo --- STARTING NETWORK
 
-broyale-qt -regtest -datadir=./node1-miner/ -rpcport=16111 -rpcuser=bitcoinrpc -rpcpassword=123456 -listen -port=17111 -connect=localhost:17222 -server -deprecatedrpc=generate &
-broyale-qt -regtest -datadir=./node2-peer/ -rpcport=16222 -rpcuser=bitcoinrpc -rpcpassword=123456 -listen -port=17222 -connect=localhost:17111 -server &
+broyale-qt -regtest -datadir=./node1-miner/ -rpcport=16111 -rpcuser=bitcoinrpc -rpcpassword=123456 -listen -port=17111 -connect=127.0.0.1:17222 -server -deprecatedrpc=generate &
+broyale-qt -regtest -datadir=./node2-peer/ -rpcport=16222 -rpcuser=bitcoinrpc -rpcpassword=123456 -listen -port=17222 -connect=127.0.0.1:17111 -server &
 sleep 4
 broyale-cli -regtest -datadir=./node1-miner/ -rpcport=16111 -rpcuser=bitcoinrpc -rpcpassword=123456 generate 101
 
